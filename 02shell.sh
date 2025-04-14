375e59fa1a46af38a6d7208825999aa03ec64cc0
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-04-14 10:49:49 +0530

    this is mny first commit

---
+++ 02shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh : hey hai ramesh how are you brother"
+echo "suresh : hmm hai i am good bro " 
+echo "Ramesh : what do you do suresh "
+echo "suresh : yeah Ramesh i got the job right now i am doing in a job"
+echo "Ramesh : hmm thats really grat my dear brother " 
