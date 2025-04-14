79df39a48a9dd2a05b54a3448931bc6f38076d13
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-04-14 18:21:49 +0530

    some msg

---
+++ 04-veriables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$!
+PERSON2=$2
+
+
+echo "$PERSON1 : hey hai $PERSON2 how are you brother"
+echo "$PERSON2 : hmm hai i am good bro " 
+echo "$PERSON1 : what do you do $PERSON2 "
+echo "$PERSON2: yeah $PERSON1 i got the job right now i am doing in a job"
+echo "$PERSON1: hmm thats really grat my dear brother " 
