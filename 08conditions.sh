8f7ddd6f423c715469e4f2caa6ed1a80adf8646b
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-04-17 17:06:55 +0530

    hello chandu

--- 08conditions.sh
+++ 08conditions.sh
@@ -9,3 +9,11 @@ else
     echo "Given number $NUMBER IS less THAN 10"
 fi
 
+NUMBER=$1
+
+if [ $NUMBER -gt 15 ]
+then 
+    echo "given number $NUMBER is greter than 15"
+else
+    echo "givn number $NUMBER IS LESS THAN 15"
+fi
