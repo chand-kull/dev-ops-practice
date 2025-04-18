eddaafdcd6f75ec287e36683583630d2bad9c2df
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-04-18 11:20:03 +0530

    hello chandu

---
+++ 11sh.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u )    
+
+if [ $USERID -ne 0 ]
+then
+    echo " please run this script with root access."
+    exit 1 
+else
+    echo "you are super user."
+fi
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "installation of mysqll...FALURE"
+    exit 1
+else
+    echo "installation of mysql...SUCCESS"
+fi  
+ 
+ dnf install maven -y
+if [ $? -ne 0 ]
+then 
+    echo "installation of maven...FALURE"
+    exit 1
+else
+    echo "installation of maven...SUCCESS"
+fi
+
+
+dnf install git -y
+if [ $? -ne 0 ]
+then 
+    echo "installation of git...FALURE"
+    exit 1
+else
+    echo "installation of git...SUCCESS"
+fi
+
+ echo "is script procceding?"
